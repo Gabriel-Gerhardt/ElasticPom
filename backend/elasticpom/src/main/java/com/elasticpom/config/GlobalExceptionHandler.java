acba139e9efd6e8ca41980f3c540a40e4624e4c0
Gabriel-Gerhardt <gabrielgerhardt27@gmail.com>
2026-05-26 10:08:14 -0300

    GAB-16: fixing exception throw

--- backend/elasticpom/src/main/java/com/elasticpom/config/GlobalExceptionHandler.java
+++ backend/elasticpom/src/main/java/com/elasticpom/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.elasticpom.config;
 
 import com.elasticpom.exception.BadRequestException;
+import com.elasticpom.exception.PaperNotInElasticException;
 import org.springframework.data.elasticsearch.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,11 @@ public class GlobalExceptionHandler {
              ApiResponse apiResponse = new ApiResponse(HttpStatus.BAD_REQUEST.value(), "Request of the resource is invalid", ex.getMessage());;
              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
          }
+    @ExceptionHandler(PaperNotInElasticException.class)
+    public ResponseEntity<ApiResponse> handleNotFound(PaperNotInElasticException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ApiResponse(404, e.getMessage(), "There is no such papers in elastic for this query"));
+    }
 
 }
--- backend/elasticpom/src/main/java/com/elasticpom/core/service/PaperService.java
+++ backend/elasticpom/src/main/java/com/elasticpom/core/service/PaperService.java
@@ -30,7 +30,7 @@ public class PaperService {
         List<String> paperIds = elasticRepository.findByQuery(query, pageable).stream().map(ElasticPaperDocument::getId).toList();
         System.out.println(paperIds);
         if (paperIds.isEmpty()) {
-            throw new PaperNotInElasticException("There is no paper in the elastic for the page" + page);
+            throw new PaperNotInElasticException("There is no paper in the elastic for the page " + page + " and this query " + query);
         }
         return getPapersByIds(paperIds);
     }
@@ -41,7 +41,7 @@ public class PaperService {
         List<String> paperIds = elasticRepository.findAll(pageable).stream().map(ElasticPaperDocument::getId).toList();
 
         if (paperIds.isEmpty()) {
-            throw new PaperNotInElasticException("There is no paper in the elastic for the page" + page);
+            throw new PaperNotInElasticException("There is no paper in the elastic for the page " + page);
         }
         return getPapersByIds(paperIds);
     }
