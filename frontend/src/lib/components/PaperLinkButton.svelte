5d69fbfda5132f85fa38327563e7c675a492a1bd
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-06-17 16:30:48 +0000

    GAB-26: add PaperLinkButton component

---
+++ frontend/src/lib/components/PaperLinkButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let link: string | null;
+</script>
+
+{#if link}
+	<a class="btn-primary text-sm inline-block" href={link} target="_blank" rel="noopener noreferrer">
+		View official page
+	</a>
+{/if}
