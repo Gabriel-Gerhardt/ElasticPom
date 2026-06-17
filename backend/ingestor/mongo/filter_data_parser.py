d4b9dff8a4e3ad28218ebd0ba53525b676b6fecd
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-06-17 12:27:57 +0000

    GAB-25: use clean filter names, swap contributors->creators

--- backend/ingestor/mongo/filter_data_parser.py
+++ backend/ingestor/mongo/filter_data_parser.py
@@ -1,5 +1,5 @@
 FILTERS = [
-    {"filtername": "subjects.keyword",     "order": 1, "type": "option"},
-    {"filtername": "contributors.keyword", "order": 2, "type": "option"},
-    {"filtername": "date",                 "order": 3, "type": "range"},
+    {"filtername": "subjects",  "order": 1, "type": "option"},
+    {"filtername": "creators",  "order": 2, "type": "option"},
+    {"filtername": "date",      "order": 3, "type": "range"},
 ]
