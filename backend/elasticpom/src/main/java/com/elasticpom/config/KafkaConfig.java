30d6c985d4d86600bd3c55bf4d9ddec2de823c81
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-07-22 21:18:48 -0300

    feat: kafka configuration in spring

---
+++ backend/elasticpom/src/main/java/com/elasticpom/config/KafkaConfig.java
@@ -0,0 +1,26 @@
+package com.elasticpom.config;
+
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    private KafkaProperties kafkaProperties;
+
+    public KafkaConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = kafkaProperties.buildConsumerProperties();
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+}
