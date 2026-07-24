fbf87aab3e29515d917c130c6144787780590c00
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-07-23 22:05:10 -0300

    feat: kafka listener

--- backend/elasticpom/src/main/java/com/elasticpom/external/integration/KafkaPaperListener.java
+++ backend/elasticpom/src/main/java/com/elasticpom/external/integration/KafkaPaperListener.java
@@ -1,19 +1,37 @@
 package com.elasticpom.external.integration;
 
+import com.elasticpom.adapters.PaperMapper;
+import com.elasticpom.adapters.dto.PaperDto;
+import com.elasticpom.core.service.ParserService;
+import com.elasticpom.external.document.ElasticPaperDocument;
+import com.elasticpom.external.document.PaperDocument;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class KafkaPaperListener {
+    private final ObjectMapper objectMapper;
+    private final PaperMapper paperMapper;
+    private final ParserService parserService;
+
+    public KafkaPaperListener(ObjectMapper objectMapper, PaperMapper paperMapper, ParserService parserService) {
+        this.objectMapper = objectMapper;
+        this.paperMapper = paperMapper;
+        this.parserService = parserService;
+    }
 
     @KafkaListener(
             topics = "paper",
             groupId = "paper-service-consumer"
     )
 
-    public void consume(String paper)  {
-
-        System.out.println("Consuming paper " + paper);
+    public void consume(String payload)  {
+        PaperDto dto = objectMapper.readValue(payload, PaperDto.class);
+        PaperDocument paperMongoDocument = paperMapper.fromDtoToDocument(dto);
+        ElasticPaperDocument elasticPaperDocument = paperMapper.fromDtoToElasticDocument(dto);
+        parserService.saveMongoDocument(paperMongoDocument);
+        parserService.saveElasticDocument(elasticPaperDocument);
     }
 
 }
